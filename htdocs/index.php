306dfbb7d5b82d205f3d03e311f76f1d62a6a1f7
Krane-13 <83793463+Krane-13@users.noreply.github.com>
2024-04-04 09:37:38 -0300

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Index</title>
+</head>
+<header>
+    <div class="header">
+        <div class="logo">
+            <div class="logoimg"><img src="assets/img/logo2.png"></div>
+        </div>
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviço</a></li>
+                    <li><a href="">Quem somos</a></li>
+                    <li><a href="">Contatos</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="login">
+            <button>LOGIN
+        </div>
+        <div class="agendar">
+            <button> Agendar horário
+        </div>
+    </div>
+</header>
+
+<body>
+    <div class="principal">
+        
+        <h1>ESTILO É UM REFLEXO DA SUA ATITUDE E SUA PERSONALIDADE.</h1>
+        <h2>Disponível 24 horas, todos os dias.</h2>
+        <button>Agendar Horário</button>
+    </div>
+    <div class="segundo">
+        <h1>sobre</h1>
+        <h2>Bem-vindo ao nosso site de agendamentos de barbearia! Aqui na Cortes Command, oferecemos uma plataforma
+            simples e conveniente para você marcar seu próximo corte de cabelo ou barba</h2>
+        <h2>Com apenas alguns cliques, você pode agendar seu horário, garantindo um serviço rápido e eficiente. Estamos
+            comprometidos em tornar o processo de agendamento fácil e sem complicações.</h2>
+        <h2><b>Disponível 24 horas, todos os dias.</b></h2>
+    </div>
+    <div class="barbeiros">
+        <h1>barbeiros<h1>
+                <h2>Lorem ipsum dolor sit amet consectetur. Cras ac senectus venenatis sem pellentesque. Quis libero sed
+                    volutpat dolor dictumst non dui egestas ipsum. Ut massa turpis proin sed.</h2>
+                <img src="">
+                <img src="">
+                <img src="">
+    </div>
+    <div class="mais_avaliados">
+        <h1>mais avaliados</h1>
+        <img src="">
+        <button>insta
+        <button>face
+        <button>twiter
+        <img src="">
+        <button>insta
+        <button>face
+        <button>twiter
+        <img src="">
+        <button>insta
+        <button>face
+        <button>twiter
+    </div>
+    <div class="unidades">
+        <img src="">
+        <!-- puxar informação -->
+        <img src="">
+        <!-- puxar informação -->
+
+    </div>
+<footer>
+    <img>insta
+    <img>face
+    <img>twiter
+    <div class="logoimg"><img src="assets/img/logo2.png"></div>
+</footer>
+</body>
+
+
+</html>
